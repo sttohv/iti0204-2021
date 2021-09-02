1d7a6f8dd56e28b3b1c41ab320fc4a1fbaad5385
sttohv <sttohv@ttu.ee>
2021-09-02 14:48:01 +0300

    Ex1

--- AL01A/src/ee/ttu/algoritmid/fibonacci/AL01A.java
+++ AL01A/src/ee/ttu/algoritmid/fibonacci/AL01A.java
@@ -1,13 +1,27 @@
 package ee.ttu.algoritmid.fibonacci;
 
+import java.math.BigInteger;
+
 public class AL01A {
     /**
      * Compute the Fibonacci sequence number.
+     *
      * @param n The number of the sequence to compute.
      * @return The n-th number in Fibonacci series.
      */
     public String iterativeF(int n) {
         // TODO
-        return "";
+        BigInteger a = new BigInteger("0");
+        BigInteger b = new BigInteger("1");
+        if (n == 0) {
+            return a.toString();
+        }
+        for (int i = 2; i < n + 1; i++) {
+            BigInteger c = a.add(b);
+            a = b;
+            b = c;
+
+        }
+        return b.toString();
     }
 }

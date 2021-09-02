7f4477181943f840f834df15a58d80abaa3c72ca
sttohv <sttohv@ttu.ee>
2021-09-02 14:22:06 +0300

    Mall

---
+++ AL01A/src/ee/ttu/algoritmid/fibonacci/AL01A.java
@@ -0,0 +1,13 @@
+package ee.ttu.algoritmid.fibonacci;
+
+public class AL01A {
+    /**
+     * Compute the Fibonacci sequence number.
+     * @param n The number of the sequence to compute.
+     * @return The n-th number in Fibonacci series.
+     */
+    public String iterativeF(int n) {
+        // TODO
+        return "";
+    }
+}

c98f3ccb45f4d9a19574bbcae72361a8bc836e28
sttohv <sttohv@ttu.ee>
2021-10-14 16:37:05 +0300

    al04 - only one side

--- AL04/src/ee/ttu/algoritmid/subtreedifference/SubtreeDifference.java
+++ AL04/src/ee/ttu/algoritmid/subtreedifference/SubtreeDifference.java
@@ -26,6 +26,7 @@ public class SubtreeDifference {
             calculateDifferences(leftChild);
             long leftChildrenSum = leftChild.getSumOfAllChildren() + leftChild.getValue();
             rootNode.setSumOfAllChildren(leftChildrenSum);
+            rootNode.setDifferenceOfLeftAndRight(leftChildrenSum);
 
         }
         else if(rightChild != null){
@@ -33,6 +34,7 @@ public class SubtreeDifference {
             long rightChildrenSum = rightChild.getSumOfAllChildren() + rightChild.getValue();
 
             rootNode.setSumOfAllChildren(rightChildrenSum);
+            rootNode.setDifferenceOfLeftAndRight(-rightChildrenSum);
 
 
         }
@@ -53,21 +55,25 @@ public class SubtreeDifference {
          *             10         17
          *           /   \       /  \
          *         3     13     5    25
+         *                 \
+         *
          */
         Node rootNode = new Node(15);
         Node a = new Node(10);
         Node b = new Node(17);
         Node c = new Node(3);
         Node d = new Node(13);
+        Node g = new Node(2);
         Node e = new Node(5);
         Node f = new Node(25);
 
         rootNode.setLeft(a);
-        rootNode.setRight(b);
+        //rootNode.setRight(b);
         a.setLeft(c);
         a.setRight(d);
         b.setLeft(e);
         b.setRight(f);
+        //c.setRight(g);
 
         SubtreeDifference solution = new SubtreeDifference();
         solution.calculateDifferences(rootNode);
@@ -75,15 +81,16 @@ public class SubtreeDifference {
         System.out.println(a.getSumOfAllChildren());
         System.out.println(b.getSumOfAllChildren());
         System.out.println(rootNode.getSumOfAllChildren());
+        System.out.println(rootNode.getDifferenceOfLeftAndRight());
 
-        if (rootNode.getDifferenceOfLeftAndRight() != -21 ||
-                a.getDifferenceOfLeftAndRight() != -10 ||
-                b.getDifferenceOfLeftAndRight() != -20 ||
-                c.getDifferenceOfLeftAndRight() != 0) {
-            throw new Exception("There is a mistake in your solution.");
-        }
-
-        System.out.println("Your solution should be working fine in basic cases, try to push.");
+//        if (rootNode.getDifferenceOfLeftAndRight() != -21 ||
+//                a.getDifferenceOfLeftAndRight() != -10 ||
+//                b.getDifferenceOfLeftAndRight() != -20 ||
+//                c.getDifferenceOfLeftAndRight() != 0) {
+//            throw new Exception("There is a mistake in your solution.");
+//        }
+//
+//        System.out.println("Your solution should be working fine in basic cases, try to push.");
 
     }
 }
